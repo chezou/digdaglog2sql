752087dd8f5432fb6a47c233f161327717cca2be
Aki Ariga <chezou+github@gmail.com>
2022-05-09 17:45:02 -0700

    Ensure having  at the end of queries

--- tests/test_digdaglog2sql.py
+++ tests/test_digdaglog2sql.py
@@ -38,7 +38,8 @@ from (
 ALTER TABLE tbl1.cdp_tmp_behavior_behavior_1 RENAME TO tbl1.behavior_behavior_1;
 DROP TABLE IF EXISTS tmp_customers;
 CREATE TABLE tmp_customers (customer_id VARCHAR)
-  WITH (bucketed_on = array['customer_id'], bucket_count = 512);
+  WITH (bucketed_on = array['customer_id'], bucket_count = 512)
+;
 INSERT INTO TABLE `tmp_customers`
 select
     m.customer_id,
@@ -51,7 +52,53 @@ from (
       td_last(`email`, time) as `email`
     from `source_tbl`.`users`
     group by 1
-  ) m"""
+  ) m
+;"""
+
+    assert extract_sql(log) == expected_sql
+
+
+def test_extractg_sql_with_comment():
+    log = """2020-11-11 01:29:29.949 +0000 [INFO] (0538@[10778:db1]+customers) io.digdag.core.agent.OperatorManager: td>: 
+2020-11-11 01:29:30.489 +0000 [INFO] (0538@[10778:db1]+customers) io.digdag.standards.operator.td.TdOperatorFactory$TdOperator: Started presto job id=884667004:
+-- project_name: db1
+-- workflow_name: audience
+-- task_name: +customers
+INSERT INTO "tmp_customers"
+-- workflow task: +customers
+select
+    m.customer_id,
+    m.`td_client_id`,
+    m.`email`
+from (
+    select
+      customer_id,
+      td_last(`td_client_id`, time) as `td_client_id`,
+      td_last(`email`, time) as `email`
+    from `source_tbl`.`users`
+    group by 1
+  ) m
+-- set session join_distribution_type = 'PARTITIONED'"""  # noqa
+
+    expected_sql = """-- project_name: db1
+-- workflow_name: audience
+-- task_name: +customers
+INSERT INTO "tmp_customers"
+-- workflow task: +customers
+select
+    m.customer_id,
+    m.`td_client_id`,
+    m.`email`
+from (
+    select
+      customer_id,
+      td_last(`td_client_id`, time) as `td_client_id`,
+      td_last(`email`, time) as `email`
+    from `source_tbl`.`users`
+    group by 1
+  ) m
+-- set session join_distribution_type = 'PARTITIONED'
+;"""  # noqa
 
     assert extract_sql(log) == expected_sql
 
@@ -98,12 +145,12 @@ ALTER TABLE tbl1.cdp_tmp_behavior_behavior_1 RENAME TO tbl1.behavior_behavior_1;
 
 DROP TABLE IF EXISTS tmp_customers;
 CREATE TABLE tmp_customers (customer_id VARCHAR)
-  WITH (bucketed_on = array['customer_id'], bucket_count = 512);
+  WITH (bucketed_on = array['customer_id'], bucket_count = 512)
+;
 
 
 2022-04-30 07:00:11.215 +0000 [INFO] (0420@[1:tbl1:76144781:411862721]+customers) io.digdag.core.agent.OperatorManager: td>: 
 
-2022-04-30 07:00:12.138 +0000 [INFO] (0634@[1:tbl1:76144781:411862721]+customers) io.digdag.standards.operator.td.TdOperatorFactory$TdOperator: Started hive job id=1372905245:
 INSERT INTO TABLE `tmp_customers`
 select
     m.customer_id,
@@ -116,6 +163,7 @@ from (
       td_last(`email`, time) as `email`
     from `source_tbl`.`users`
     group by 1
-  ) m"""  # noqa
+  ) m
+;"""  # noqa
 
     assert extract_td_sql(log) == expected_output
